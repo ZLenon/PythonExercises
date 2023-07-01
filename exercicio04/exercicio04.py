e93c5ca4bd2010af8ae8194c3a9502fbeead3b08
Lenon <lenoninformatica@gmail.com>
2023-07-01 17:48:06 -0300

    Feat: Exercicio 04

--- exercicio04/exercicio04.py
+++ exercicio04/exercicio04.py
@@ -1,13 +1,13 @@
 # Crie um programa que leia algo e mostre na tela seus tipo primitivo e
 # informações sobre ele
 
-n1 = int(input("Digite algo "))
+n1 = input("Digite algo ")
 
 print(f"O tipo primitivo é {type(n1)}")
-print(f"So tem espaços? {n1}")
-print(f"É um numero? {n1}")
-print(f"É alphanumerico? {n1}")
-print(f"Esta em maiusculo? {n1}")
-print(f"Está em minusculo? {n1}")
-print(f"O tipo primitivo é {n1}")
-print(f"Esta capitalizado? {n1}")
+print(f"So tem espaços? {n1.isspace()}")
+print(f"É um numero? {n1.isnumeric()}")
+print(f"É alfabetico? {n1.isalpha()}")
+print(f"É alfanumerico? {n1.isalnum()}")
+print(f"Esta em maiusculo? {n1.isupper()}")
+print(f"Está em minusculo? {n1.islower()}")
+print(f"Esta capitalizado? {n1.istitle()}")
