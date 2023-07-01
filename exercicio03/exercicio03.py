9360b2c0dfc7cd1bc43e1fd06910541ff20bc09f
Lenon <lenoninformatica@gmail.com>
2023-07-01 17:33:43 -0300

    Feat: Exercicio 03

--- exercicio03/exercicio03.py
+++ exercicio03/exercicio03.py
@@ -1,8 +1,8 @@
 # Crie um programa que leia dois numeros e imprima a soma deles
 
-n1 = int(input('Digite um valor '))
-n2 = int(input('Digite segundo valor '))
+n1 = int(input("Digite um valor "))
+n2 = int(input("Digite segundo valor "))
 
 soma = n1 + n2
 
-print(f'A soma entre {n1} e {n2} vale {soma}')
+print(f"A soma entre {n1} e {n2} vale {soma}")
