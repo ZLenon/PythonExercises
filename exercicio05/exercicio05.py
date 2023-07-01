37004deb6dd11cd6c7dc2cb14f2e4ba6ae0906be
Lenon <lenoninformatica@gmail.com>
2023-07-01 18:35:35 -0300

    Feat: Exercicio05

--- exercicio05/exercicio05.py
+++ exercicio05/exercicio05.py
@@ -1,2 +1,6 @@
 # Faça um programa que leia um numero inteiro
 # e mostre na tela seu sucessor e seu antecessor
+
+numeInt = int(input("Digite um Numero inteiro - "))
+
+print(f"Seu sucessor é {numeInt + 1} e seu antecessor é {numeInt - 1}")
