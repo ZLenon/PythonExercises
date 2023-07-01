ca934c5399f51932e8f06fc99a0ac55b36b1d6f6
Lenon <lenoninformatica@gmail.com>
2023-07-01 17:19:43 -0300

    Feat: Exercicio 03 ler dois numeros e imprimir a soma deles

---
+++ exercicio04/exercicio04.py
@@ -0,0 +1,12 @@
+# Crie um programa que leia algo e mostre na tela seus tipo primitivo e informações sobre ele
+
+n1 = int(input('Digite algo '))
+
+print(f'O tipo primitivo é {type(n1)}')
+print(f'So tem espaços? {n1}')
+print(f'É um numero? {n1}')
+print(f'É alphanumerico? {n1}')
+print(f'Esta em maiusculo? {n1}')
+print(f'Está em minusculo? {n1}')
+print(f'O tipo primitivo é {n1}')
+print(f'Esta capitalizado? {n1}')
