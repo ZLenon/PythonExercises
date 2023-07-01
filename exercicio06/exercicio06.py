7b9771a7c9c3c60c4193893b04338057828007ce
Lenon <lenoninformatica@gmail.com>
2023-07-01 18:44:20 -0300

    Feat: Exercicio06

--- exercicio06/exercicio06.py
+++ exercicio06/exercicio06.py
@@ -1,2 +1,6 @@
 # crie um algoritimo que leia um numero
 # e mostre o seu dobro, triplo e raiz quadrada
+
+numInt = int(input("Digite um numero inteiro - "))
+
+print(f"O dobro de {numInt} é {numInt * 2} e sua raiz quadrada é {numInt ** 2}")
