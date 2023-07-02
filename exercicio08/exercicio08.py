bb6b79f0fcfcf657db53cd29ee7c9c92a8d423e8
Lenon <lenoninformatica@gmail.com>
2023-07-02 15:16:35 -0300

    Feat: exercicio 08

--- exercicio08/exercicio08.py
+++ exercicio08/exercicio08.py
@@ -1,2 +1,19 @@
 # Escreva um programa que leia o valor em metros
-# e o exiba convertido para centimetros
+# e o exiba convertido para centimetros, milimitros,
+# decimetro,decametro,hecometro, kilometro
+
+metros = float(input("Digite a QTD em Metros - "))
+km = metros * 0.001
+hm = metros * 0.01
+dam = metros * 0.1
+dm = metros * 10
+cm = metros * 100
+mm = metros * 1000
+
+print(f"Essa QTD {metros} m - metros corresponde a: ")
+print(f" {cm} cm - centimetros")
+print(f" {mm} mm - milimetros")
+print(f" {dm} dm - decimetros")
+print(f" {dam} dam - decametro")
+print(f" {hm} hm - Hectometro")
+print(f" {km} km - kilometro")
