0e78f3b0145c693d45ed09f982f7471d2cef6d31
Lenon <lenoninformatica@gmail.com>
2023-07-02 15:42:13 -0300

    Feat: exercicio 10

--- exercicio10/exercicio10.py
+++ exercicio10/exercicio10.py
@@ -1,3 +1,9 @@
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira
 # e mostre quantos Dolares ela pode comprar.
 # Considere o DOLAR USS1.00 = R$3.27
+
+dinheiro = float(input("Quanto dinheiro voce possui? R$ "))
+real = 3.27
+dolar = dinheiro / real
+
+print("Com R${:.2f} voce pode comprar U${:.2f}".format(dinheiro, dolar))
