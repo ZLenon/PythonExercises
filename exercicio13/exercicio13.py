3d06ec9ce1c00ab7229aa37ac516a84ce6b9d277
Lenon <lenoninformatica@gmail.com>
2023-07-02 16:43:50 -0300

    Feat: exercicio 13

--- exercicio13/exercicio13.py
+++ exercicio13/exercicio13.py
@@ -1,2 +1,10 @@
 # Faça um algoritimo que leia o salario de um funcionario e
 # mostre seu novo salario com 15% de aumento
+
+salario = float(input("Digite seu salario - R$"))
+aumento = 0.15  # 15% - 15 / 100
+salario_com_aumento = salario * aumento
+
+print("Salario - R${}".format(salario))
+print("Aumento R${:.2f}".format(salario_com_aumento))
+print("Novo Salario R${:.2f}".format(salario_com_aumento + salario))
