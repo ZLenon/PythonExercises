7713cc75b0dace5f76dafa987d7e4029e74759ce
Lenon <lenoninformatica@gmail.com>
2023-07-02 16:52:36 -0300

    Feat: exercicio 14

---
+++ exercicio14/exercicio14.py
@@ -0,0 +1,6 @@
+# Crie um algoritimo que converta a temperatura C° de para F°
+
+temp = float(input("Informe a temperatura - °C"))
+celcius = ((9 * temp) / 5) + 32
+
+print("A temperatuda em °F é de {:.2f}".format(celcius))
