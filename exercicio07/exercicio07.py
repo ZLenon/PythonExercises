61a72c1d1c77ea00497600037ded9c4836495d34
Lenon <lenoninformatica@gmail.com>
2023-07-02 15:01:16 -0300

    Feat: exercicio 07

--- exercicio07/exercicio07.py
+++ exercicio07/exercicio07.py
@@ -1,2 +1,7 @@
 # Desenvolva um programa que leia as duas notas de um aluno
 # calcule e mostre a sua média
+
+nota1 = float(input("Digite a nota 1 - "))
+nota2 = float(input("Digite a nota 2 - "))
+
+print(f"A media entre {nota1} e {nota2} é de {(nota1 + nota2) / 2}")
