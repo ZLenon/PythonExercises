7ede24f3e07c827e09f913552ae113c96a30955a
Lenon <lenoninformatica@gmail.com>
2023-07-02 15:25:53 -0300

    Feat: exercicio 9

--- exercicio09/exercicio09.py
+++ exercicio09/exercicio09.py
@@ -1,2 +1,9 @@
 # Faça um programa que leia um numero inteiro
 # e mostre na tela a sua tabuada
+
+numInt = int(input("Digite um numero inteiro"))
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for num in numeros:
+    result = numInt * num
+    print(f"{numInt} * {num} = {result}")
