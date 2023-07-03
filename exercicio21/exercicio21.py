2b46b71eacc2a311b65913b0009618cdd4dc5c2c
Lenon <lenoninformatica@gmail.com>
2023-07-02 22:22:57 -0300

    Feat: exercicio 21

---
+++ exercicio21/exercicio21.py
@@ -0,0 +1,11 @@
+# Faça um programa em python que abra e reproduza o audio de um arquivo mp3
+# import pyglet
+
+# Cria uma instância da classe Audio para o arquivo MP3
+# musica = pyglet.media.load("musica.mp3")
+
+# Reproduz a música
+# musica.play()
+
+# Mantém o programa em execução até que a música termine
+# pyglet.app.run()
