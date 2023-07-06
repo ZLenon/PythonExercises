c0812f5c17ef8ab62a71d5424558d184af2f39fb
Lenon <lenoninformatica@gmail.com>
2023-07-05 22:04:13 -0300

    Feat: exercicio 30

---
+++ exercicio30/exercicio30.py
@@ -0,0 +1,9 @@
+# Crie um programa que leia um numero inteiro
+# e mostre na tela se ele é PAR ou ÍMPAR
+
+num = int(input("Digite um numero - "))
+
+if num % 2 == 0:
+    print("Esse numero é PAR")
+else:
+    print("Esse numero é IMPAR")
