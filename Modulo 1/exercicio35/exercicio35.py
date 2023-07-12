6992b9473dea7d4d23282baefc3d47f6b53b3979
Lenon Nascimento Cardoso <lenoninformatica@gmail.com>
2023-07-11 22:06:47 -0300

    Feat: Exercicio 35

--- Modulo 1/exercicio35/exercicio35.py
+++ Modulo 1/exercicio35/exercicio35.py
@@ -1,1 +1,13 @@
+# Desenvolva um programa que leia o comprimento de três retas e diga ao
+# usuario se elas podem ou não formar um triangulo
+print("==*" * 20)
+print("Analizando tres retas de um triangulo")
+print("==*" * 20)
+reta1 = float(input("Digite a primeira reta - "))
+reta2 = float(input("Digite a segunda reta - "))
+reta3 = float(input("Digite a terceira reta - "))
 
+if reta1 < (reta2 + reta3) and reta2 < (reta3 + reta1) and reta3 < (reta2 + reta1):
+    print("Podem formar um triangulo")
+else:
+    print("Não podem formar um triangulo")
