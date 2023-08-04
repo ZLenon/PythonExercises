9c4e2987b375b4f1ef3396baf325017cfb70ba4a
Lenon Nascimento Cardoso <lenoninformatica@gmail.com>
2023-08-04 17:24:23 -0300

    Feat: exercicio47

---
+++ Modulo 2/exercicio47/exercicio47.py
@@ -0,0 +1,9 @@
+# Crie um programa que mostre na tela todos os numeros pares
+# que estão no intervalo entre 1 a 50
+print("=*" * 20)
+
+for x in range(0, 51, 2):
+    print(x, end="-")
+
+
+print("=*" * 20)
