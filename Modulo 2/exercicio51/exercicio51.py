99a6b733a28e37832cbbcd4ead7487022abd9a06
Lenon Nascimento <lenoninformatica@gmail.com>
2023-09-13 17:23:34 -0300

    Feat: exercicio51

--- Modulo 2/exercicio51/exercicio51.py
+++ Modulo 2/exercicio51/exercicio51.py
@@ -1,8 +1,9 @@
 # Desenvolva um programa que leia o primeiro termo e a razao de uma Progressao
 # aritimetica. No final, mostre os 10 primeiros termos dessa progressão
 
-first_term = int(input("Digite o primeiro termo - "))
-razao = int(input("Digite a razao aritimetica - "))
+first_term = int(input("Digite o primeiro termo: "))
+razao = int(input("Digite a razao arritimetica: "))
+ten_term = (first_term + (10 - 1) * razao) + razao
 
-for x in range(first_term, first_term + (10 - 1) * razao, razao):
+for x in range(first_term, ten_term, razao):
     print(x)
