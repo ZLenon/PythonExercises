2c7f4e22c8fad811f214ef4ddd87e92a6f663d07
Lenon Nascimento <lenoninformatica@gmail.com>
2023-09-19 16:42:16 -0300

    Feat: exercicio68

--- Modulo 2/exercicio68/exercicio68.py
+++ Modulo 2/exercicio68/exercicio68.py
@@ -8,19 +8,21 @@ print("=" * 16)
 total = vitoria = 0
 
 while True:
-    jogador = int(input("Digite um numero: "))
-    cpu = randint(0, 11)
-    total = cpu + jogador
-    par_impar = "PAR" if total % 2 == 0 else "IMPAR"
-    opcao = str(input("Escolha PAR ou IMPAR - [P/I] - ")).upper().strip()[0]
-    print(f"CPU-{cpu} Jogador-{jogador} -> {total} é {par_impar}")
-    if opcao in par_impar:
-        vitoria += 1
-        print("Venceu!!!")
-    else:
-        print("Derrota, FIM de jogo!!!")
+    try:
+        jogador = int(input("Digite um numero: "))
+        cpu = randint(0, 11)
+        total = cpu + jogador
+        par_impar = "PAR" if total % 2 == 0 else "IMPAR"
+        opcao = str(input("Escolha PAR ou IMPAR - [P/I] - ")).upper().strip()[0]
+        print(f"CPU-{cpu} Jogador-{jogador} -> {total} é {par_impar}")
+        if opcao in par_impar:
+            vitoria += 1
+            print("Venceu!!!")
+        else:
+            print("Derrota, FIM de jogo!!!")
+            print("=" * 16)
+            break
         print("=" * 16)
-        break
-    print("=" * 16)
-
+    except ValueError as x:
+        print(f"Use numero ao invez de letras. \nO Erro mostrado é - {x}")
 print(f"Total de vitorias nessa rodada - {vitoria}")
