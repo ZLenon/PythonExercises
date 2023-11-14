50412345b734a006b7ea1388cee7c2b89156961b
Lenon Nascimento <lenoninformatica@gmail.com>
2023-11-14 15:39:30 -0300

    Feat: exercicio72

--- Modulo 3/exercicio72/exercicio72.py
+++ Modulo 3/exercicio72/exercicio72.py
@@ -1,3 +1,34 @@
 # Crie um programa que tenha uma tupla totalmente preenchida com uma contagem
 # por extensão de zero ate vinte. Seu programa devera ler um numero pelo
 # teclado entre 0 a 20 e mostra por extenso
+
+
+numeracao = (
+    "zero",
+    "um",
+    "dois",
+    "tres",
+    "quatro",
+    "cinco",
+    "seis",
+    "sete",
+    "oito",
+    "nove",
+    "dez",
+    "onze",
+    "doze",
+    "treze",
+    "quatorze",
+    "quinze",
+    "dezesseis",
+    "dezessete",
+    "dezoito",
+    "dezenove",
+    "vinte",
+)
+
+while True:
+    num = int(input("Digite um numero entre 0 a 20: "))
+    if 0 <= num <= 20:
+        break
+print(f"O numero digitado é {numeracao[num]}")
