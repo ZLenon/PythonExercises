f08dd36d0c76f7252e9b4d395a2e29bb7093912d
Lenon Nascimento <lenoninformatica@gmail.com>
2023-11-20 16:11:12 -0300

    Feat: exercicio77

--- Modulo 3/exercicio77/exercicio77.py
+++ Modulo 3/exercicio77/exercicio77.py
@@ -1,2 +1,27 @@
 # Crie um programa que tenha um tupla com varios palavras.
 # Depois disso voce deve mostrar para cada palavra quais sao as suas vogais.
+
+
+palavras = (
+    "Aprender",
+    "Programar",
+    "Linguagem",
+    "Python",
+    "Curso",
+    "Gratis",
+    "Estudar",
+    "Praticar",
+    "Trabalhar",
+    "Mercado",
+    "Programador",
+    "Futuro",
+)
+count = 0
+for x in palavras:
+    print(f"\nNa palavra {x.upper()} apareceu", end=" ")
+    count += 1
+    for letra in x:
+        if letra.lower() in "aeiou":
+            print(letra.lower(), end=" ")
+    if count == len(palavras):
+        print("\nFIM!!")
