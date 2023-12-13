1849ab658a70b11657fb93b9e89d5c4a121213b7
Lenon Nascimento <lenoninformatica@gmail.com>
2023-12-13 14:17:11 -0300

    Feat: exercicio79

--- Modulo 3/exercicio79/exercicio79.py
+++ Modulo 3/exercicio79/exercicio79.py
@@ -2,3 +2,19 @@
 # e cadastre em uma lista. Caso o numero ja existe la dentro ela nao sera
 # adicionado. No final serão exibidos todos os valores unicos digitados em
 # ordem crescente
+
+lista = []
+continuar = ""
+while continuar in "S":
+    valor = int(input("Digite uma Valor: "))
+
+    if valor in lista:
+        print("Valor ja esta na lista - Nao vou adicionar")
+    else:
+        lista.append(valor)
+    print("*=" * 30)
+    continuar = str(input("Quer continuar? [S/N]: ")).upper()[0]
+
+print("*=" * 30)
+lista.sort()
+print(f"Voce digitou {lista}")
