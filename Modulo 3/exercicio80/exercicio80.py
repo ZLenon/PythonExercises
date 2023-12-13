b126b2ecd399f5596db2c5125767156c2416ccd6
Lenon Nascimento <lenoninformatica@gmail.com>
2023-12-13 20:31:44 -0300

    Feat: exercicio80

--- Modulo 3/exercicio80/exercicio80.py
+++ Modulo 3/exercicio80/exercicio80.py
@@ -1,3 +1,29 @@
 # Crie um programa onde o usuario possa digitar cinco valores numericos e
 # cadastre em uma lista. Ja na posição correta de inserção(sem usar sort).
 #  No final mostre a lista ordenada na tela
+
+# Inicializa uma lista vazia
+lista = list()
+
+# Loop para solicitar a entrada do usuário cinco vezes
+for num in range(0, 5):
+    # Solicita ao usuário para digitar um valor e converte para inteiro
+    valor = int(input("Digite um valor: "))
+
+    # Verifica se é o primeiro elemento ou se o valor é maior que o último elemento na lista
+    if num == 0 or valor > lista[-1]:
+        # Se verdadeiro, adiciona o valor ao final da lista
+        lista.append(valor)
+    else:
+        # Caso contrário, procura a posição correta para inserir o valor mantendo a ordem
+        index = 0
+        while index < len(lista):
+            # Compara o valor com cada elemento da lista
+            if valor <= lista[index]:
+                # Insere o valor na posição correta e interrompe o loop
+                lista.insert(index, valor)
+                break
+            index += 1
+
+# Imprime a lista ordenada
+print("Lista Ordenada:", lista)
