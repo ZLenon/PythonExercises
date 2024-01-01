c90e4eb5c880cbe1d613fd21848cfc33f0f53ffd
Lenon Nascimento <lenoninformatica@gmail.com>
2024-01-01 20:03:04 -0300

    Feat: exercicio86

---
+++ Modulo 3/exercicio86/exercicio86.py
@@ -0,0 +1,14 @@
+# crie um programa que crie uma matriz de dimensão 3x3 e prencha com valores
+# lidos pelo teclado. No final mostre a matriz na tela com a formatação correta
+
+
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        matriz[linha][coluna] = int(input(f"Digite o valor [{linha,coluna}]: "))
+print("=*" * 15)
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        print(f"[{matriz[linha][coluna]:^3}]", end="")
+    print()
