697c50e54f4ba5ba6a025d35747c74e4a1a72cff
Lenon Nascimento <lenoninformatica@gmail.com>
2024-01-07 15:35:20 -0300

    Feat: exercicio91

--- Modulo 3/exercicio91/exercicio91.py
+++ Modulo 3/exercicio91/exercicio91.py
@@ -7,6 +7,27 @@ from random import randint
 from time import sleep
 from operator import itemgetter
 
-time = sleep(1)
-ranking = dict({"jogador1": randint(1, 6)})
-print(ranking)
+print("-=" * 6, "Game🎲", "-=" * 6)
+game = dict(
+    {
+        "jogador1": randint(1, 6),
+        "jogador2": randint(1, 6),
+        "jogador3": randint(1, 6),
+        "jogador4": randint(1, 6),
+    }
+)
+
+for key, value in game.items():
+    print(f"{key} jogou o dado e tirou {value}")
+    sleep(1)
+
+rank = list()
+rank = sorted(game.items(), key=itemgetter(1), reverse=True)
+print("¨" * 30)
+
+print("-=" * 5, "Resultado🏆", "-=" * 5)
+for index, value in enumerate(rank):
+    if index == 0:
+        print(f"{value[0]} foi o campeão tirou 🎲{value[1]}")
+    else:
+        print(f"{value[0]} ficou em {index + 1}, tirou 🎲{value[1]}")
