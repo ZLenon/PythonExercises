f4fd81d7ce2cd771f5f4a76e4664ae4d90912aa7
Lenon Nascimento <lenoninformatica@gmail.com>
2024-01-15 15:43:41 -0300

    Feat: exercicio97

--- Modulo 3/exercicio97/exercicio97.py
+++ Modulo 3/exercicio97/exercicio97.py
@@ -1,2 +1,13 @@
-# Faça um programa que tenha uma função chamada escreva()
-# que receba um texto qualquer como paramentro e mostre uma mensagem com tamanho adaptavel
+# Faça um programa que tenha uma função chamada escreva() que receba um texto qualquer como paramentro e mostre uma mensagem com tamanho adaptavel
+
+
+def escreva(palavra):
+    tama = len(palavra)
+    print("~" * tama)
+    print(palavra)
+    print("~" * tama)
+
+
+escreva("Lenon Nascimento")
+escreva("Curso de python no youtube")
+escreva("EBL")
