922ef915e693d744bcd31a65c05590d3c963d55d
Lenon Nascimento <lenoninformatica@gmail.com>
2024-01-29 20:47:25 -0300

    Feat: exercicio110

--- Modulo 3/exercicio110/exercicio110.py
+++ Modulo 3/exercicio110/exercicio110.py
@@ -1,3 +1,5 @@
+# Adicione ao modulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que ja temos no modulo creiado ate aqui.
+
 from ex110 import resumo
 
 valor = int(input("Digite um preço: "))
