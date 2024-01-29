4285810d6df8aea9aefd1b5b13af9bd2f65add2a
Lenon Nascimento <lenoninformatica@gmail.com>
2024-01-29 20:47:16 -0300

    Feat: exercicio109

--- Modulo 3/exercicio109/exercicio109.py
+++ Modulo 3/exercicio109/exercicio109.py
@@ -1,3 +1,4 @@
+# Modifique as funções que foram criaa no desafio 107 para que eles aceitem um parametro a mais, informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida no desafio 108.
 from ex109 import juros, dobro, metade, desconto, formacted
 
 num = int(input("Digite um preço: "))
