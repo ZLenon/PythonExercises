fc18f470768445b32dfc207157c3b81b4914d636
Lenon Nascimento <lenoninformatica@gmail.com>
2024-01-29 20:47:50 -0300

    Feat: exercicio112

--- Modulo 3/exercicio112/exercicio112.py
+++ Modulo 3/exercicio112/exercicio112.py
@@ -1,3 +1,5 @@
+# Dentro do pacote utilidades que criamos no desafio 111 temos um modulo chamado dado. Crie uma função chamada leia_dinheiro() que seja capaz de funcionar como uma validação de dados para aceitar apenas valores que sejam monetarios.
+
 from utilidade import moeda, dado
 
 
