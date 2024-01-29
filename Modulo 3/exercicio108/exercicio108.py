7ccaf40492d7e2e4996e93c2a144fee5eaa82eb6
Lenon Nascimento <lenoninformatica@gmail.com>
2024-01-29 20:47:00 -0300

    Feat: exercicio108

--- Modulo 3/exercicio108/exercicio108.py
+++ Modulo 3/exercicio108/exercicio108.py
@@ -1,3 +1,4 @@
+# Adapte o codigo do desafio 107, criando uma função adicional chamada moeda() que consiga mostrar os valores como um valor monetario formatado.
 import ex108
 
 num = int(input("Digite um preço: "))
