88284557d36dd8abfc2b7a7ec182b626eae825d7
Lenon Nascimento <lenoninformatica@gmail.com>
2024-01-30 16:36:46 -0300

    Feat: exercicio114

--- Modulo 3/exercicio114/exercicio114.py
+++ Modulo 3/exercicio114/exercicio114.py
@@ -1,4 +1,22 @@
 # Crie um codigo em python que teste se o site PUDIM esta acessivel pelo computador usado.
+# https://static.ragnaplace.com/db/npc/gif/${numero}.gif
 import requests
 
-requests.get()
+
+def site_online(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+    except requests.ConnectionError:
+        return False
+
+
+num = 100
+url = f"https://api.ragnaplace.com/ro/list/mob?server=bro&type=server&categorie=all&order=id&sort=asc&page=1&limit=3&search="
+if site_online(url):
+    print("O site esta ONLINE")
+else:
+    print("O site esta OFFLINE")
