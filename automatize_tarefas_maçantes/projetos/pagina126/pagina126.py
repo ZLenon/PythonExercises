dfe25ec409347f7ea44f7e24f894982e73c2f54e
Lenon Nascimento <lenoninformatica@gmail.com>
2024-02-08 21:32:28 -0300

    Feat: pagina126

--- automatize_tarefas_maçantes/projetos/pagina126/pagina126.py
+++ automatize_tarefas_maçantes/projetos/pagina126/pagina126.py
@@ -1,5 +1,5 @@
-# Sequência de Collatz Crie uma função chamada collatz() que tenha um parâmetro de nome number. Se number for par, collatz() deverá exibir number // 2 e reatribua esse valor a number. Se number for ímpar, collatz() deverá exibir e reatribua 3 * number + 1. Em seguida, crie um programa que permita que o usuário digite um inteiro e fique chamando collatz() com esse número até a função retornar o valor 1. (De modo bastante surpreendente, essa sequência, na realidade, funciona para qualquer inteiro – cedo ou tarde, ao usar essa sequência, você chegará em 1! Até mesmo os matemáticos não têm muita certeza do motivo. Seu programa está explorando o que chamamos de sequência de Collatz, às vezes chamada de “o mais simples problema matemático impossível”.)
 # pagina 126
+
 from time import sleep
 
 
