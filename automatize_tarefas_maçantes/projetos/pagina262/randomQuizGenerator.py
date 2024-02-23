db1af83a8f4e8edf729b77b9117343e613edc780
Lenon Nascimento <lenoninformatica@gmail.com>
2024-02-23 17:26:50 -0300

    Feat: pagina262

---
+++ automatize_tarefas_maçantes/projetos/pagina262/randomQuizGenerator.py
@@ -0,0 +1,77 @@
+import random
+
+capitals = {
+    "Alabama": "Montgomery",
+    "Alaska": "Juneau",
+    "Arizona": "Phoenix",
+    "Arkansas": "Little Rock",
+    "California": "Sacramento",
+    "Colorado": "Denver",
+    "Connecticut": "Hartford",
+    "Delaware": "Dover",
+    "Florida": "Tallahassee",
+    "Georgia": "Atlanta",
+    "Hawaii": "Honolulu",
+    "Idaho": "Boise",
+    "Illinois": "Springfield",
+    "Indiana": "Indianapolis",
+    "Iowa": "Des Moines",
+    "Kansas": "Topeka",
+    "Kentucky": "Frankfort",
+    "Louisiana": "Baton Rouge",
+    "Maine": "Augusta",
+    "Maryland": "Annapolis",
+    "Massachusetts": "Boston",
+    "Michigan": "Lansing",
+    "Minnesota": "Saint Paul",
+    "Mississippi": "Jackson",
+    "Missouri": "Jefferson City",
+    "Montana": "Helena",
+    "Nebraska": "Lincoln",
+    "Nevada": "Carson City",
+    "New Hampshire": "Concord",
+    "New Jersey": "Trenton",
+    "New Mexico": "Santa Fe",
+    "New York": "Albany",
+    "North Carolina": "Raleigh",
+    "North Dakota": "Bismarck",
+    "Ohio": "Columbus",
+    "Oklahoma": "Oklahoma City",
+    "Oregon": "Salem",
+    "Pennsylvania": "Harrisburg",
+    "Rhode Island": "Providence",
+    "South Carolina": "Columbia",
+    "South Dakota": "Pierre",
+    "Tennessee": "Nashville",
+    "Texas": "Austin",
+    "Utah": "Salt Lake City",
+    "Vermont": "Montpelier",
+    "Virginia": "Richmond",
+    "Washington": "Olympia",
+    "West Virginia": "Charleston",
+    "Wisconsin": "Madison",
+    "Wyoming": "Cheyenne",
+}
+
+path = "./automatize_tarefas_maçantes/projetos/pagina262/"
+
+for quizNum in range(35):
+    # Cria os arquivos com as provas e os gabaritos das respostas.
+    quizFile = open(path + "capitalsquiz%s.txt" % (quizNum + 1), "w")
+    answerKeyFile = open(path + "capitalsquiz_answers%s.txt" % (quizNum + 1), "w")
+    # Escreve o cabeçalho da prova.
+    quizFile.write("Name:\n\nDate:\n\nPeriod:\n\n")
+    quizFile.write((" " * 20) + "State Capitals Quiz (Form %s)" % (quizNum + 1))
+    quizFile.write("\n\n")
+
+# Embaralha a ordem dos estados.
+states = list(capitals.keys())
+random.shuffle(states)
+for questionNum in range(50):
+    # Obtém respostas corretas e incorretas.
+    correctAnswer = capitals[states[questionNum]]
+    wrongAnswers = list(capitals.values())
+    del wrongAnswers[wrongAnswers.index(correctAnswer)]
+    wrongAnswers = random.sample(wrongAnswers, 3)
+    answerOptions = wrongAnswers + [correctAnswer]
+    random.shuffle(answerOptions)
