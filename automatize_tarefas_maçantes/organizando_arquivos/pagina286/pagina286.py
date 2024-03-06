c211dd0c263dfe10fd8d1e6d3c564f2b31961e27
Lenon Nascimento <lenoninformatica@gmail.com>
2024-03-05 21:49:29 -0300

    Feat: pagina286

--- automatize_tarefas_maçantes/organizando_arquivos/pagina286/pagina286.py
+++ automatize_tarefas_maçantes/organizando_arquivos/pagina286/pagina286.py
@@ -1,4 +1,8 @@
-from zipfile import ZipFile
+from zipfile import ZipFile, ZIP_DEFLATED
 
-with ZipFile("new.zip", "w") as file:
-    file.write("spam.txt", compress_type=ZipFile.ZIP_DEFLATED)
+base = "./automatize_tarefas_maçantes/organizando_arquivos/pagina286/"
+
+arquivo = base + "pixel_art.zip"
+
+with ZipFile(arquivo, "w") as file:
+    file.write("spam.txt", compress_type=ZIP_DEFLATED)
