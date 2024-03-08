056070e4b95220aceaf60e8c4ea8a2d5b0da590f
Lenon Nascimento <lenoninformatica@gmail.com>
2024-03-08 15:57:36 -0300

    Feat: pagina287

---
+++ automatize_tarefas_maçantes/projetos/pagina287/renameDates.py
@@ -0,0 +1,36 @@
+import os, re
+
+datePattern = re.compile(
+    r"""^(.*?) # todo o texto antes da data
+((0|1)?\d)- # um ou dois dígitos para o mês
+((0|1|2|3)?\d)- # um ou dois dígitos para o dia
+((19|20)\d\d) # quatro dígitos para o ano
+(.*?)$ # todo o texto após a data
+""",
+    re.VERBOSE,
+)
+path = "./automatize_tarefas_maçantes/projetos/pagina287/"
+
+for file_name in os.listdir(path):
+    mo = datePattern.search(file_name)
+    print(file_name)
+    if mo == None:
+        continue
+
+    beforePart = mo.group(1)
+    monthPart = mo.group(2)
+    dayPart = mo.group(4)
+    yearPart = mo.group(6)
+    afterPart = mo.group(8)
+    print("Antes: ", beforePart)
+    print("Depois: ", afterPart)
+    print("Mes: ", monthPart)
+    print("Dia: ", dayPart)
+    print("Ano: ", yearPart)
+    euroFilename = beforePart + dayPart + "-" + monthPart + "-" + yearPart + afterPart
+    print("Nome padrão EUA: ", euroFilename)
+    absWorkingDir = os.path.abspath(".")
+    amerFilename = os.path.join(absWorkingDir, euroFilename)
+    print("Caminho: ", amerFilename)
+    os.rename(file_name, euroFilename)
+    print("=*" * 20)
