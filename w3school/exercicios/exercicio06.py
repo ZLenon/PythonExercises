62d02ff794e2b45ef160de4b4d9336417ecf08e7
Lenon Nascimento <lenoninformatica@gmail.com>
2024-03-30 15:46:22 -0300

    Feat: exercicio06

--- w3school/exercicios/exercicio06.py
+++ w3school/exercicios/exercicio06.py
@@ -5,6 +5,15 @@ class StringsClasse:
     def percorre_string(self, string: str, index: int) -> str:
         return string[index]
 
-    def percorre_com_for(self,string: str) -> str:
+    def percorre_com_for(self, string: str) -> str:
         for x in string:
             return x
+
+    def tamanho_string(self, string: str) -> int:
+        return len(string)
+
+    def verifica_texto_string(self, string):
+        if "lenon" in str(string).lower():
+            return "Sim, seu nome esta no texto"
+        if "lenon" not in str(string).lower():
+            return "Não, seu nome não esta no texto"
--- w3school/test/test_exercicio06.py
+++ w3school/test/test_exercicio06.py
@@ -11,3 +11,17 @@ def test_percorre_c_for():
     classe = exercicio06.StringsClasse()
     metodo = classe.percorre_com_for("testando")
     assert metodo == "t"
+
+
+def test_tamanho_string():
+    classe = exercicio06.StringsClasse()
+    metodo = classe.tamanho_string("lenon")
+    assert metodo == 5
+
+
+def test_veriica_string_no_texto():
+    classe = exercicio06.StringsClasse()
+    correct_case = classe.verifica_texto_string("me chamo Lenon")
+    assert correct_case == "Sim, seu nome esta no texto"
+    incorrect_case = classe.verifica_texto_string("me chamo mikey")
+    assert incorrect_case == "Não, seu nome não esta no texto"
